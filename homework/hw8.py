856146dee5d773d4353ddb32e5c43fefa5067223
NothingZX <99936410+NothingZX@users.noreply.github.com>
2024-01-14 00:39:37 +0800

    Add files via upload

---
+++ homework/hw8.py
@@ -0,0 +1,10 @@
+1. Andrew Ng
+Founder and CEO of Landing AI, Founder of deeplearning.ai. 
+
+Website: https://www.andrewng.org, Twitter: @AndrewYNg, Facebook: Andrew Ng, Google Scholar. 
+
+Andrew was a co-founder and head of Google Brain. He was also the Chief Scientist at Baidu and led the company’s AI group. He is a pioneer in online education as a co-founder of deeplearning.ai and Coursera – the world’s largest MOOC platform, which started off with more than 100,000 students enrolling for his popular courseCS229A. Dr Ng has touched countless lives through his work as a computer scientist which led to him being named as one of Time magazine’s 100 most influential people in 2012.  
+
+Dr Ng’s research is mainly in fields such as Machine learning, deep learning, computer vision, machine perception and natural language processing. His papers which frequently won the best paper award at academic conferences, eventually made him hugely popular and influential among computer scientists and had a massive influence in the field of AI, robotics and computer vision. 
+
+Some of his most well-known work as one of the top AI Leaders include his Autonomous Helicopter Project at Stanford and the Stanford Artificial Intelligence Robot project, which ended up producing an open-source robotics software platform that is widely used today. The Google brain project, which he founded in 2011, used artificial neural networks that were trained using deep learning. The distributed computer with 16,000 CPU cores learnt how to recognize catches from watching YouTube videos and not being taught what a cat really is. The technology which comes from the project is still used in the speech recognition system of Android Operating Systems. 
